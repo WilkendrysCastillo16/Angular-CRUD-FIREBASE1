3cfdeb1941057d8344f35490dfcb55a9d35869e3
WilkendrysCastillo16 <84988672+WilkendrysCastillo16@users.noreply.github.com>
2021-11-02 00:51:14 -0300

    Agregando y visualizando Firebase cli al listado

--- src/app/Componentes/list-tareas/list-tareas.component.ts
+++ src/app/Componentes/list-tareas/list-tareas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { TareasService } from 'src/app/Servicios/tareas.service';
 
 @Component({
   selector: 'app-list-tareas',
@@ -8,13 +9,29 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list-tareas.component.css']
 })
 export class ListTareasComponent implements OnInit {
+  Tareas: any[] = [];
 
-
-  constructor() { 
-
+  constructor(private _tareasservice:TareasService) { 
+    
   }
 
   ngOnInit(): void {
+    this.getTareas();
+  }
+
+  getTareas(){
+    this._tareasservice.GetTareas().subscribe(data =>{
+      this.Tareas = [];
+     data.forEach((element:any) => {
+         /*console.log(element.payload.doc.id);*/
+
+        this.Tareas.push({
+          id:element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.Tareas);
+    });
   }
 
 }
--- src/app/Servicios/tareas.service.ts
+++ src/app/Servicios/tareas.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -11,6 +12,10 @@ export class TareasService {
 
   agregarTarea(Tarea:any):Promise<any> {
     return this.firestore.collection("Tarea").add(Tarea);
+  }
+
+  GetTareas():Observable<any>{
+    return this.firestore.collection("Tarea", ref => ref.orderBy("FechadeCreacion",'asc')).snapshotChanges();
   }
 
 }
